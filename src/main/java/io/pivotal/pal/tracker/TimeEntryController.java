a8d5256db203d166713b2593e40164fa4705a453
neelamari <neelamari@icloud.com>
2018-07-11 13:39:42 -0600

    actuator changes

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,28 +1,41 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 @RestController
 @RequestMapping("/time-entries")
 public class TimeEntryController {
 
     private TimeEntryRepository timeEntryRepository;
+    private CounterService counterService;
+    private GaugeService gaugeService;
 
-    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+    public TimeEntryController(
+            TimeEntryRepository timeEntryRepository,
+            CounterService counterService,
+            GaugeService gaugeService) {
         this.timeEntryRepository = timeEntryRepository;
+        this.counterService = counterService;
+        this.gaugeService = gaugeService;
     }
 
     @GetMapping
     public ResponseEntity<List<TimeEntry>> list() {
+        counterService.increment("Time.Listed");
         return new ResponseEntity<>(timeEntryRepository.list(),HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntryToCreate) {
+        counterService.increment("Time.Created");
+        gaugeService.submit("Time.NumberOfEntries", timeEntryRepository.list().size());
         return new ResponseEntity<TimeEntry>(timeEntryRepository.create(timeEntryToCreate),HttpStatus.CREATED);
     }
 
@@ -31,8 +44,8 @@ public class TimeEntryController {
        TimeEntry timeEntry = timeEntryRepository.find(id);
        if (timeEntry == null) {
            return new ResponseEntity<TimeEntry>(HttpStatus.NOT_FOUND);
-
        } else {
+           counterService.increment("Time.Read");
            return new ResponseEntity<TimeEntry>(timeEntry, HttpStatus.OK);
        }
     }
@@ -41,6 +54,7 @@ public class TimeEntryController {
     public ResponseEntity<TimeEntry> update(@PathVariable Long id, @RequestBody TimeEntry timeEntry) {
         TimeEntry updatedTimeEntry = timeEntryRepository.update(id, timeEntry);
         if (updatedTimeEntry != null) {
+            counterService.increment("Time.Updated");
             return new ResponseEntity<>(updatedTimeEntry, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -50,6 +64,9 @@ public class TimeEntryController {
     @DeleteMapping("{id}")
     public ResponseEntity<TimeEntry> delete (@PathVariable long id) {
         timeEntryRepository.delete(id);
+        counterService.increment("Time.Deleted");
+        gaugeService.submit("Time.NumberOfEntries", timeEntryRepository.list().size());
         return new ResponseEntity<TimeEntry>(HttpStatus.NO_CONTENT);
     }
 }
+
---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryHealthIndicator.java
@@ -0,0 +1,28 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TimeEntryHealthIndicator implements HealthIndicator {
+
+    private final TimeEntryRepository timeEntryRepo;
+
+    public TimeEntryHealthIndicator(TimeEntryRepository timeEntryRepo) {
+        this.timeEntryRepo = timeEntryRepo;
+    }
+
+    @Override
+    public Health health() {
+        Health.Builder builder = new Health.Builder();
+
+        if (timeEntryRepo.list().size() < 5) {
+            builder.up();
+        } else {
+            builder.down();
+        }
+
+        return builder.build();
+    }
+}
